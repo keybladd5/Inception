d74328108a220d76248f319f12ca7d19d94ee342
inception <polmarti@Inception.inception.polmarti.org>
2025-07-17 21:23:01 +0200

    fix: alt mode to init mysql

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-#Script to 
-if [ ! -d /var/lib/mysql/${MYSQL_DATABASE}];	# Checks if the database already exists
+set -ex 
+
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ];	# Checks if the database already exists
 then
 	mysql_install_db # Creates /var/lib/mysql directory structure
-	service mysql start # Start MySQL service (alternative: mysqld_safe & to launch in background)
+	mysqld_safe --skip-networking & # Start MySQL service
 	sleep 5 # Must wait for database to be ready 
-	mysql -u ${MYSQL_ROOT_USER} -p ${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE;" #Creates the specified database
+	mysql -u ${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE $MYSQL_DATABASE;" #Creates the specified database
 	mysql -e "CREATE USER'$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" #Creates a new user with access from any host (%)
 	mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;" #Grants full permissions to user on the database
 	mysql -e "FLUSH PRIVILEGES;" #Updates privileges table immediately
@@ -14,4 +15,4 @@ then
 	mysqladmin -u ${MYSQL_ROOT_USER} --password=${MYSQL_ROOT_PASSWORD} shutdown #Shuts down MySQL properly
 fi
 
-mysqld #Starts MySQL server in foreground
+mysqld_safe #Starts MySQL server in foreground
